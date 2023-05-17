e2f36b55e0d85ef95c0e25c00d62d92072082556
imgustacabral <imgustacabral@gmail.com>
2023-05-17 20:14:03 -0300

    refactor: show capitalized pokemon name

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -4,21 +4,22 @@ interface PokemonCardProps {
 }
 
 export function PokemonCard({ name, abilities }: PokemonCardProps) {
+  const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+
   return (
-    <div className="bg-gray-100 rounded w-60 mx-auto flex flex-col gap-4">
-      <div className=" bg-gray-200 rounded-t-lg w-full h-1/5 p-6  flex justify-center items-center">
-        <p className="font-bold text-gray-700">{name}</p>
+    <div className="bg-gray-50 rounded w-60 mx-auto flex flex-col gap-4">
+      <div className=" bg-gray-100 rounded-t-lg w-full h-1/5 p-6  flex justify-center items-center">
+        <p className="font-bold text-gray-700">{capitalizedName}</p>
       </div>
 
-        <p>Habilidades:</p>
-        {abilities.map((ability: string, index: number) => {
-          return (
-            <div className="text-center p-2" key={index}>
-              <p className="border-b border-b-gray-200 px-4">{ability}</p>
-            </div>
-            
-          );
-        })}
+      <p className="text-center">Habilidades:</p>
+      {abilities.map((ability: string, index: number) => {
+        return (
+          <div className="text-center p-2" key={index}>
+            <p className="border-b border-b-gray-200 px-4">{ability}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
